e32c9ab06ec9bc87a00e1e740998a2327c4a71a3
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-18 14:12:51 -0400

    Add project files.

---
+++ Topic 7.1 - While Loops ASSIGNMENT/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Topic_7._1___While_Loops_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+
+
+        }
+    }
+}
