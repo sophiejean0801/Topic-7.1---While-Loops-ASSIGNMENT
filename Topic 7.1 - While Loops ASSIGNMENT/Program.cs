e7f08914631035f052b3041ac16a22a9f3adbacd
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-18 14:24:14 -0400

    unfinished, i hate loops :(

--- Topic 7.1 - While Loops ASSIGNMENT/Program.cs
+++ Topic 7.1 - While Loops ASSIGNMENT/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Topic_7._1___While_Loops_ASSIGNMENT
+﻿using System.Net.Security;
+
+namespace Topic_7._1___While_Loops_ASSIGNMENT
 {
     internal class Program
     {
@@ -6,7 +8,9 @@
         {
 
             Console.ForegroundColor = ConsoleColor.Red;
-
+            int num1, num2;
+            Console.WriteLine("Welcome to the loser casino! You will be playing a game of chance. You will be asked to guess heads or tails. Ready?");
+            
 
 
         }
