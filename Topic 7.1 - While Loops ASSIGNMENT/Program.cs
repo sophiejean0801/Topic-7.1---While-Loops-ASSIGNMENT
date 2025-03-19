ee244d697829cecb77ed6a86fdf97a8af5b7ce02
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-19 14:03:11 -0400

    loops suck

--- Topic 7.1 - While Loops ASSIGNMENT/Program.cs
+++ Topic 7.1 - While Loops ASSIGNMENT/Program.cs
@@ -8,11 +8,59 @@ namespace Topic_7._1___While_Loops_ASSIGNMENT
         {
 
             Console.ForegroundColor = ConsoleColor.Red;
-            int num1, num2;
-            Console.WriteLine("Welcome to the loser casino! You will be playing a game of chance. You will be asked to guess heads or tails. Ready?");
-            
+            bool done = false;
+            int points = 3;
+            Console.WriteLine("Welcome to the worst casino! You will be playing a game of chance. ");
+            while (points > 0)
+            {
+                Console.WriteLine("Enter heads or tails");
+                int coin = rand.Next(0, 2);
+                
+                if ()
+                {
+                    Console.WriteLine("Heads");
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                }
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Random rand = new Random();
+                int num2 = rand.Next(1, 7);
+                if (num1 == num2)
+                {
+                    Console.WriteLine("Great job! You get to keep your points!!");
+                }
+                else
+                {
+                    Console.WriteLine("You suck! You lose 1 point!");
+                    points--;
+                }
+                Console.WriteLine("You have " + points + " points.");
+
+                Console.WriteLine("Would you like to continue? Who knows, you might get lucky.");
+                string answer = Console.ReadLine();
+                if (answer == "no")
+                {
+                    Console.WriteLine("Coward.");
+                    done = true;
+                }
+                else
+                {
+                    done = false;
+                }
+                while (points == 0)
+                {
+                    Console.WriteLine("You lose. How does it feel to fail?");
+                    done = true;
+
+                }
+                
 
 
+
+
+            }
         }
     }
 }
