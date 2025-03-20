01a9143ff0bba9055b844fd0f53808e8d4db1e7c
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-20 13:47:36 -0400

    jkbfKJVB

--- Topic 7.1 - While Loops ASSIGNMENT/Program.cs
+++ Topic 7.1 - While Loops ASSIGNMENT/Program.cs
@@ -11,39 +11,62 @@ namespace Topic_7._1___While_Loops_ASSIGNMENT
             Console.ForegroundColor = ConsoleColor.Red;
             bool done = false;
             string guess;
-            int flip, heads = 0, tails = 1; 
+            int flip, points = 3, heads = 0, tails = 1; 
             Random random = new Random();
 
            // heads is 0 and tails is 1
 
-            int points = 3;
             Console.WriteLine("Welcome to the worst casino! You will be playing a game of chance. ");
             while (!done)
             {
                 Console.WriteLine("Enter heads or tails");
                 guess = Console.ReadLine();
                 flip = random.Next(0, 2);
-                
-                if (guess == "heads" && guess == heads)
+
+                if (guess == "heads" && flip == 0)
                 {
-                   
-
+                    Console.WriteLine("The coin landed on heads! You win!");
+                    Console.WriteLine("You now have " + points + " points!");
                 }
-                if (guess == "tails")
+                if (guess == "tails" && flip == 1 )
                 {
-                    
+                    Console.WriteLine("The coin landed on tails! You win!");
+                    Console.WriteLine("You now have " + points + " points!");
                 }
-
-                if (flip == flip2)
+                if (guess == "heads" && flip == 1)
                 {
-                    Console.WriteLine("You're actually somewhat lucky! Look at you go!");
+                    Console.WriteLine("You guessed heads and the coin landed on tails! You suck!");
+                    points--;
+                    Console.WriteLine("You now have " + points + " points!");
                 }
-                
-                
+                if (guess == "tails" && flip == 0)
+                {
+                    Console.WriteLine("You guessed tails and the coin landed on heads! You suck!");
+                    points--;
+                    Console.WriteLine("You now have " + points + " points!");
+                }
+                Console.WriteLine("Would you like to continue?");
+                if (Console.ReadLine() == "no")
+                {
+                    Console.WriteLine("Coward.");
+                    done = true;
+                }
+                if (Console.ReadLine() == "yes")
+                {
+                    Console.WriteLine("Good for you. You don't suck that much.");
+                    done = false;
+                }
+                while (points == 0)
+                {
+                    Console.WriteLine("You have no more points! You loser!");
+                    done = true;
+                }
 
 
-                
-                       
+
+
+
+
             }
         }
     }
